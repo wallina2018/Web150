a2fac9da4b1e7e86c430ade30ed135d2524ecd02
Andrew Wallin <awwallin63@gmail.com>
2018-01-09 09:23:02 -0800

    update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var 
+//In the external script.js file, define variables for firstName, lastName, yearsOfStudy, and goal. 
+
+//Once you've created these files, commit them to your local repository and then to gitHub.com. Then submit the github.com url //for your index.html file in Canvas.
+
+var firstName = "Andrew";
+var lastName = "Wallin";
+var yearsOfStudy = 1;
+var goal = "Web Design AAS degree";
+
